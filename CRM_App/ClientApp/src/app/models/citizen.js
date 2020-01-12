36eb93bc284bac4f20d98e7c8cf563a0ce9047aa
Harry <hatsav@micro.com>
2020-01-12 04:52:42 +0200

    Add Angular Material and Models

---
+++ CRM_App/ClientApp/src/app/models/citizen.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Citizen = /** @class */ (function () {
+    function Citizen() {
+    }
+    return Citizen;
+}());
+exports.Citizen = Citizen;
+//# sourceMappingURL=citizen.js.map
---
+++ CRM_App/ClientApp/src/app/models/citizen.ts
@@ -0,0 +1,16 @@
+export class Citizen {
+  public CitizenID: number;
+  public AMKA: string;
+  public AFM: string;
+  public Onoma: string;
+  public Eponimo: string;
+  public Patronimo: string;
+  public Mitronimo: string;
+  public HmerominiaGenisis: Date;
+  public PoliGenisis;
+  public Tilefono: string;
+  public Kinito: string;
+  public eMail: string;
+  public login: string;
+  public password: string;
+}
